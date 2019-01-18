2a74f0b922db7af62287ddd515767149c8ccde30
Jasper King-Harman <j.kingharman@googlemail.com>
2019-01-18 12:55:44 +0000

    Refactor promise

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -7,7 +7,7 @@ class WeatherContainer extends Component {
 
 	constructor(props) {
   	super(props);
-  	this.state = {currentWeather: "default", currentLocation: "London"};
+  	this.state = {currentWeather: "", currentLocation: "London"};
     this.handleChange = this.handleChange.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
@@ -24,38 +24,27 @@ class WeatherContainer extends Component {
   	this.setState({currentLocation: e.target.value})
   }
 
-  parseResponse(res) {
-  	return {
-    	weather: res.weather[0].icon,
-      temp: this.toCelcius(res.main.temp)
-    }
-  }
-
 	toCelcius(kelvin) {
 		return (kelvin - 273).toFixed()
 	}
 
+	parseResponse(json) {
+		if (json.cod == "200") {
+			this.setState({currentWeather: {weather: json.weather[0].icon, temp: this.toCelcius(json.main.temp)}})
+		} else if (json.cod == "404") {
+			this.setState({currentWeather: {weather: "unknown", temp: "unknown"}})
+		} else {
+			console.log("error")
+		}
+	}
+
 
 	getWeather() {
-   // refactor promise structure when writing for real request
-  	let response = new Promise((resolve) => {
-    let url = "https://api.openweathermap.org/data/2.5/weather?q=" + this.state.currentLocation + "&APPID=d67b6ea31078ccea4bd77846d9569c02"
-
-    resolve(fetch(url))
-    });
-
-    response.then(res => {
+  	new Promise((resolve) => {
+    	resolve(fetch("https://api.openweathermap.org/data/2.5/weather?q=" + this.state.currentLocation + "&APPID=d67b6ea31078ccea4bd77846d9569c02"))
+		}).then(res => {
   		res.json()
-      	.then(json => {
-          if (json.cod == "200") {
-						console.log(json)
-            this.setState({currentWeather: this.parseResponse(json)})
-          } else if (json.cod == "404") {
-            this.setState({currentWeather: {weather: "unknown", temp: "unknown"}})
-          } else {
-            console.log("error")
-          }
-        })
+      	.then(this.parseResponse.bind(this))
     })
   };
 
