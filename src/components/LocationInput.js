2ec1d6f1265b70601e6d89e0905ff89d19fef48a
Jasper King-Harman <j.kingharman@googlemail.com>
2019-01-21 22:03:04 +0000

    Prevent default behaviour for form

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class LocationInput extends Component {
+
+  stopDefault(e) {
+    e.preventDefault()
+  }
   render() {
     return (
-      <form className='location-input'>
+      <form className='location-input' onSubmit={this.stopDefault}>
         <input placeholder="Enter city" type="text" onChange={this.props.onChange} />
       </form>
     );
